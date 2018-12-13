274e5c59b6d84e62cbde4960d8ef5d3fb46e093d
yt_suzuki <>
2018-12-13 18:14:17 +0900

    変更

--- practice/chapter7/chap7_2_location/chap7_2_location.js
+++ practice/chapter7/chap7_2_location/chap7_2_location.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('btn').addEventListener('click', function() {
         let obj = document.getElementById('number');
         let val = obj.value;
-        location.href = 'file:///Users/y_suzuki/development/JavaScript/practice/chapter7/' + val;
+        // ファイルサーバー実行時
+        // location.href = 'file:///Users/y_suzuki/development/JavaScript/practice/chapter7/' + val;
+
+        // 200 OK 実行時　（200OKの指定フォルダを chapter07 に指定
+        location.href = 'http://127.0.0.1:8887/' + val;
     }, false)
 }, false);
 
@@ -13,7 +17,7 @@ var result = document.getElementById('result');
 // カウントアップボタンを押した時に履歴を追加
 document.getElementById('cnt').addEventListener('click', function() {
     result.textContent = ++count;
-    history.pushState(count, null, '/js/chap07/count/' + count);
+    history.pushState(count, null, 'chap7_2_location.html' + count);
 });
 
 // 戻るボタンでページの状態を前に戻す

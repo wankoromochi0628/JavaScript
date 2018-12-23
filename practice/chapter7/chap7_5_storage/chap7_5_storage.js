4cf9463e9f2d3b97280b7157ceada4c5efe580ef
鈴木優人 <wankoromoti0628@gmail.com>
2018-12-24 02:35:23 +0900

    変更

--- practice/chapter7/chap7_5_storage/chap7_5_storage.js
+++ practice/chapter7/chap7_5_storage/chap7_5_storage.js
@@ -1,10 +1,11 @@
+// ローカルストレージ
+var storage = localStorage;
+
 // ストレージの保存
 document.getElementById('save').addEventListener('click', function() {
-    var txt1 = document.getElementById('text1');
-    var txt2 = document.getElementById('text2');
-    var txt3 = document.getElementById('text3');
-
-    var storage = localStorage;
+    var txt1 = document.getElementById('text1').value;
+    var txt2 = document.getElementById('text2').value;
+    var txt3 = document.getElementById('text3').value;
 
     // 保存
     storage.setItem('item1', txt1);
@@ -12,15 +13,14 @@ document.getElementById('save').addEventListener('click', function() {
     storage['item3'] = txt3;
 
     // 取得
-    console.log(storage.getItem('item1'));
-    console.log(storage.item2);
-    console.log(storage['item3']);
+    // console.log(storage.getItem('item1'));
+    // console.log(storage.item2);
+    // console.log(storage['item3']);
 });
 
 // ストレージの取得
 document.getElementById('out').addEventListener('click', function() {
-    var storage = sessionStorage;
-    
+    // 取得
     console.log(storage.getItem('item1'));
     console.log(storage.item2);
     console.log(storage['item3']);
